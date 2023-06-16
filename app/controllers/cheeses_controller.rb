32723c1719bd02f1595beb847acf6869043bf728
Nkatha_Glory <kinyua6945@gmail.com>
2023-06-16 23:13:34 +0300

    Done

---
+++ app/controllers/cheeses_controller.rb
@@ -0,0 +1,12 @@
+class CheesesController < ApplicationController
+    def index
+        byebug
+      end
+
+      def index
+        cheeses = Cheese.all
+        render json: cheeses
+      end
+      
+      
+end
